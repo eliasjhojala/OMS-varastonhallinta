a6d4cdc722210ca215c55016e990ff7586db2edf
Elias Ojala <elias.jh.ojala@gmail.com>
2017-08-12 18:10:39 +0300

    Revert "Added non-working mysql things"
    This reverts commit 2fc4d04c25573f7ae4118b1f8c82c79b210cd4c7.

--- mysql.php
+++ mysql.php
@@ -1,24 +1,32 @@
 <?php
+include 'auth.php';
+$db = new mysqli($servername, $username, $password, $dbname);
 
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
 
-  include 'auth.php';
-  $db = new mysqli($servername, $username, $password, $dbname);
+?>
 
-  if($db->connect_errno > 0){
-      die('Unable to connect to database [' . $db->connect_error . ']');
+<?php
+
+function newUser($first_name, $last_name, $nickname, $member_id) {
+$sql = <<<SQL
+  INSERT INTO `users` (id, first_name, last_name, nick_name, member_id)
+  VALUES (NULL, 'test', 'teste', 'test', 222)
+SQL;
+  
+  echo "TOIMIIKOKOFUNKTIOJOTENKIN<br>";
+  
+  if($db->query($sql) === TRUE){
+    echo "EI TOIMI";
   }
-
-
-  $sql = "INSERT INTO users (first_name, last_name, nick_name)
-  VALUES ('TESTIHENKILOTOIMIIKOHANTAMA', 'Doe', 'john@example.com')";
-    
-    echo "TOIMIIKOKOFUNKTIOJOTENKIN<br>";
-    
-    if ($db->query($sql) === TRUE) {
-      echo "TOIMII";
-    }
-    $db->close();
-
+  else {
+    echo "TOIMII";
+  }
+  echo "<br>Toimii vertailun jälkeen<br>";
+  mysqli_close($db);
+}
 
 
 ?>
