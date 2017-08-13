8a4f46a9f8e3a004f69239888e9272141d45e6c3
leevironty <30956409+leevironty@users.noreply.github.com>
2017-08-13 18:59:30 +0300

    Testinapin lisäys

--- siteContent.php
+++ siteContent.php
@@ -1,3 +1,6 @@
 <div id="siteContent">
-  TESTI SITECONTENT
+  <div style="padding-top:64px;"><!-- Nopea ja ruma ratkaisu. Tulee korjata tulevaisuudessa-->
+
+  </div>
+  <div class="ajax"><button class="contactFormButton">Käyttäjät</button></div>
 </div>
