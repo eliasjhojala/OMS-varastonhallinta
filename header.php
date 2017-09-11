fcad68502c6382bffe5d5c4f8e2711e917e8cdf1
leevironty <30956409+leevironty@users.noreply.github.com>
2017-09-11 20:52:57 +0300

    Lisätty vanhat muutokset, ei mitän hajua sisällöstä

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
     <div class="title"><h1>Varastonhallinta</h1></div>
     <div class=logo><img src="assets/logo-512-bgvihree.png" alt="OMS-logo"></div>
     <nav class="site-nav">
-        <button>Nappi 1</button>
-        <button>Nappi 2</button>
-        <button>Nappi 3</button>
-        <button>Dropdown</button>
+        <button><h2>Nappi 1</h2></button>
+        <button><h2>Nappi 2</h2></button>
+        <button><h2>Nappi 3</h2></button>
+        <button><h2>Dropdown</h2></button>
     </nav>
 </header>
--- siteContent.php
+++ siteContent.php
@@ -1,11 +1,33 @@
 <div id="siteContent">
+
+
     <div class="own">
-        <h3>Lainassa</h3>
+        <h3 class="subtitle">Lainassa</h3>
         <div class="well list">
             <!-- Malliesine js:ää varten -->
-            <div class="none own-item-sample own-item">
+            <div class="none own-item-sample own-item active">
                 <span class="counter">1</span>
                 <p class="name">Saha</p>
+                <div class="dropdown hidden">
+                    <button>Nappi 1</button>
+                    <button>Nappi 2</button>
+                    <button>Nappi 3</button>
+                </div>
+            </div>
+
+            <div class="none own-item-sample own-item">
+                <span class="counter">4</span>
+                <p class="name">Kirves</p>
+                <div class="dropdown hidden">
+                    <button>Nappi 1</button>
+                    <button>Nappi 2</button>
+                    <button>Nappi 3</button>
+                </div>
+            </div>
+
+            <div class="none own-item-sample own-item">
+                <span class="counter">145</span>
+                <p class="name">Naula</p>
                 <div class="dropdown hidden">
                     <button>Nappi 1</button>
                     <button>Nappi 2</button>
@@ -16,6 +38,8 @@
 
         </div>
     </div>
+
+
     <div class="item">
         <!-- Päivitetään js:llä ja ajaxilla-->
         <div class="status">
@@ -23,15 +47,17 @@
             <p class="updating">Päivittää</p>
             <p class="error">Virhe</p>
         </div>
+        <div class="formwrapper">
+            <div class="iteminput">
+                <input class="item_id" type="text" name="item_id"  autofocus placeholder="Syötä viivakoodi skannerilla tai käsin">
+                <input class="send" type="submit" value="Siirrä">
+            </div>
 
-        <form>
-            <input type="text" name="item_id"  autofocus placeholder="Syötä viivakoodi skannerilla tai käsin">
-            <input class="send" type="submit" value="Siirrä">
-        </form>
-        <form>
-            <button>Muutama</button>
-            <button>nappi</button>
-            <button>usko jo!</button>
+            <div class="typebuttons">
+                <button class="ok active">Kunnossa</button>
+                <button class="fixable">Korjattavissa</button>
+                <button class="broken">Tosi rikki</button>
+            </div>
 
             <textarea class="new-entry" name="new_entry" rows="8" cols="80" placeholder="Lisää halutessasi merkintä esimerkiksi esineen käytettävyydestä"></textarea>
             <input type="submit" value="Merkitse">
@@ -43,12 +69,27 @@
                     Kahva meni rikki
                 </p>
 
+                <p class="none log-entry type-fixed">
+                    <span class="timestamp">12.1.2017</span>
+                    Löydettiin sutattuna ja rikottuna ja vaikka mitä...
+                </p>
+
+                <p class="none log-entry type-fixed">
+                    <span class="timestamp">31.12.2016</span>
+                    Joku neropatti oli tehnyt sitä ja tätä.
+                </p>
+
             </div>
-        </form>
+
+        </div>
     </div>
+
+
     <div class="inventory">
-        <button class="tab"><h4>Varattu</h4></button>
-        <button class="tab"><h4>Varastossa</h4></button>
+        <div class="tabs">
+            <button class="tab"><h4>Varattu</h4></button>
+            <button class="tab"><h4>Varastossa</h4></button>
+        </div>
         <div class="well list">
             <!-- Malliesine js:ää varten -->
             <div class="none inventory-item-sample inventory-item">
@@ -60,9 +101,28 @@
                     <button>Nappi 3</button>
                 </div>
             </div>
+
+            <div class="none inventory-item-sample inventory-item">
+                <span class="counter">8</span>
+                <p class="name">niger</p>
+                <div class="dropdown hidden">
+                    <button>Nappi 1</button>
+                    <button>Nappi 2</button>
+                    <button>Nappi 3</button>
+                </div>
+            </div>
+
+            <div class="none inventory-item-sample inventory-item">
+                <span class="counter">24</span>
+                <p class="name">bigfun</p>
+                <div class="dropdown hidden">
+                    <button>Nappi 1</button>
+                    <button>Nappi 2</button>
+                    <button>Nappi 3</button>
+                </div>
+            </div>
             <!-- Tähän haetaan ajaxilla dataa joka muokataan js:llä kivaan muotoon-->
 
         </div>
-
     </div>
 </div>
