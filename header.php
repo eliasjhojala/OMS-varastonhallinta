e4a28ca4f027cd3901b5d2a11c80afb2f990c142
unknown <leevi.ronty@gmail.com>
2017-09-24 10:41:22 +0300

    Fixattiin napit

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <header class="site-header">
     <div class="title"><h1>Varastonhallinta</h1></div>
     <div class=logo><img src="assets/logo-512-bgvihree.png" alt="OMS-logo"></div>
-    <nav class="site-nav">
-        <button><h2>Nappi 1</h2></button>
-        <button><h2>Nappi 2</h2></button>
-        <button><h2>Nappi 3</h2></button>
-        <button><h2>Dropdown</h2></button>
+    <nav class="site-nav buttonBar">
+        <button>Nappi 1</button>
+        <button>Nappi 2</button>
+        <button>Nappi 3</button>
+        <button>Dropdown</button>
     </nav>
 </header>
