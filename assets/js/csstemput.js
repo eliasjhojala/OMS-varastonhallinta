abaa39f512597dbf55a3c70e4327dbd43bf37f7f
unknown <leevi.ronty@gmail.com>
2017-09-24 12:30:59 +0300

    Togglausjuttuja

--- assets/js/csstemput.js
+++ assets/js/csstemput.js
@@ -3,5 +3,7 @@ $(function() {
     $(".own-item").removeClass("active");
     $(this).toggleClass("active");
   });
-
+  $(".own-item.list-toggler").click(function(){
+    $(this).siblings(".item-list").toggleClass("hidden")
+  })
 });
