71c1f7178c928c91b5ab9af4f0551073d83df6c8
Elias Ojala <elias.jh.ojala@gmail.com>
2017-09-24 11:29:29 +0300

    Edited navbar

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
 <?php beginHeader("site-header"); ?>
     <div class="title"><h1>Varastonhallinta</h1></div>
-    <div class="logo"><img src="white-logo-with-alpha-512.png" alt="OMS-logo"></div>
-    <nav class="site-nav buttonBar">
+    <div id="linkBarLinks">
       <?php
-        button("Nappi 1");
-        button("Nappi 2");
-        button("Nappi 3");
-        button("Dropdown");
+        makeLink("Nappi 1", "");
+        makeLink("Nappi 2", "");
+        makeLink("Nappi 3", "");
+        makeLink("Dropdown", "");
       ?>
     </nav>
 <?php endHeader(); ?>
