6bf3e084af0fd1f2a1f6398a31c0220eaa5b27fe
Elias Ojala <elias.jh.ojala@gmail.com>
2017-09-24 15:24:22 +0300

    More cleanup

--- siteContent.php
+++ siteContent.php
@@ -4,52 +4,7 @@
     <div class="own">
         <h3 class="subtitle">Lainassa</h3>
         <div class="well list">
-            <!-- Malliesine js:ää varten -->
-            <div class="none own-item-sample own-item active">
-                <span class="counter">1</span>
-                <p class="name">Saha</p>
-                <div class="dropdown hidden">
-                    <button>Poista kaikki</button>
-                    <button class="list-toggler">Näytä yksitellen</button>
-                    <div class="item-list hidden">
-                      <p class="unique-item">Saha 123<button class="remove">Poista</button></p>
-                    </div>
-                </div>
-
-            </div>
-            <template id="own-item-template">
-              <div class="none own-item-sample own-item active">
-                  <span class="counter">1</span>
-                  <p class="name">Saha</p>
-                  <div class="dropdown hidden">
-                      <button>Poista kaikki</button>
-                      <button class="list-toggler">Näytä yksitellen</button>
-                      <div class="item-list hidden">
-                        <p class="unique-item">Saha 123<button class="remove">Poista</button></p>
-                      </div>
-                  </div>
-              </div>
-            </template>
-
-            <div class="none own-item-sample own-item">
-                <span class="counter">4</span>
-                <p class="name">Kirves</p>
-                <div class="dropdown hidden">
-                  <button>Poista kaikki</button>
-                  <button>Näytä yksitellen</button>
-                </div>
-            </div>
-
-            <div class="none own-item-sample own-item">
-                <span class="counter">145</span>
-                <p class="name">Naula</p>
-                <div class="dropdown hidden">
-                    <button>Poista kaikki</button>
-                    <button>Näytä yksitellen</button>
-                </div>
-            </div>
-            <!-- Tähän haetaan ajaxilla dataa joka muokataan js:llä kivaan muotoon-->
-
+          <?php showItems(getItems()); ?>
         </div>
     </div>
 
@@ -63,38 +18,124 @@
             <button class="tab"><h4>Varastossa</h4></button>
         </div>
         <div class="well list">
-            <!-- Malliesine js:ää varten -->
-            <div class="none inventory-item-sample inventory-item">
-                <span class="counter">3</span>
-                <p class="name">Puolijoukkueteltta</p>
-                <div class="dropdown hidden">
-                    <button>Nappi 1</button>
-                    <button>Nappi 2</button>
-                    <button>Nappi 3</button>
-                </div>
-            </div>
-
-            <div class="none inventory-item-sample inventory-item">
-                <span class="counter">8</span>
-                <p class="name">niger</p>
-                <div class="dropdown hidden">
-                    <button>Nappi 1</button>
-                    <button>Nappi 2</button>
-                    <button>Nappi 3</button>
-                </div>
-            </div>
-
-            <div class="none inventory-item-sample inventory-item">
-                <span class="counter">24</span>
-                <p class="name">bigfun</p>
-                <div class="dropdown hidden">
-                    <button>Nappi 1</button>
-                    <button>Nappi 2</button>
-                    <button>Nappi 3</button>
-                </div>
-            </div>
-            <!-- Tähän haetaan ajaxilla dataa joka muokataan js:llä kivaan muotoon-->
-
+          <?php showItems(getItems()); ?>
         </div>
     </div>
 </div>
+
+
+<?php
+  function getItems() {
+    $items = array(
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15)),
+      array("count" => "45", "name" => "kirves", "item-list" =>
+      array(42, 646, 23, 34, 12)),
+      array("count" => "3", "name" => "saha", "item-list" =>
+      array(4, 87, 23, 34, 15))
+    );
+    return $items;
+  }
+?>
+
+<?php
+  function showItems($items) {
+    foreach($items as $singleItem) {
+      itemSample($singleItem);
+    }
+  }
+?>
+
+<?php
+  function itemSample($item) { ?>
+    <div class="none own-item-sample own-item">
+        <span class="counter"><?php echo $item["count"]; ?></span>
+        <p class="name"><?php echo $item["name"]; ?></p>
+        <div class="dropdown hidden">
+            <button>Poista kaikki</button>
+            <button class="list-toggler">Näytä yksitellen</button>
+            <div class="item-list hidden">
+              <ul>
+                <?php foreach($item["item-list"] as $unique_item_id) { ?>
+                  <li class="unique-item"><?php echo $item["name"]." ".$unique_item_id; ?><button class="remove">Poista</button></li>
+                <?php } ?>
+              </ul>
+            </div>
+        </div>
+    </div>
+  <?php }
+?>
