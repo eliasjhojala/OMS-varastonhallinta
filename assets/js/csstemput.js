b59e09fb9addcb4d9ad0e7ef022661afa25f6604
Elias Ojala <elias.jh.ojala@gmail.com>
2017-09-28 17:53:05 +0300

    Edits

--- assets/js/csstemput.js
+++ assets/js/csstemput.js
@@ -10,7 +10,7 @@ $(function() {
 
 
   $(".tab-button").click(function(){
-    var elementName = $(this).attr('id').split("-")[0] + "-tab-content";
+    var elementName = "#" + $(this).attr('id').split("-")[0] + "-tab-content";
     $(this).parent().siblings().addClass("hidden");
     $(elementName).removeClass("hidden");
   });
