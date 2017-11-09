b120a935c76c682414badb489e8ff416bd327882
Elias Ojala <elias.jh.ojala@gmail.com>
2017-11-09 18:48:18 +0200

    Add validation things (still in progress)

--- functions.php
+++ functions.php
@@ -1,3 +1,5 @@
+<?php include 'mysqlFunctions.php'; ?>
+
 <?php
 
 function button($text, $class=null, $id=null) {
@@ -19,6 +21,44 @@ function endHeader() {
 function radioButton($name, $value, $text) {
   echo '<input type="radio" id="'.$name.$value.$text.'" name="'.$name.'" value="'.$value.'">';
   echo '<label for="'.$name.$value.$text.'">'.$text.'</label>';
+}
+
+function validated() {
+  $session_id = 2;
+  $sql = "SELECT * FROM sessions WHERE 'session_id'=$session_id";
+  $result = sqlQuery($sql);
+  if($result != null) {
+    global $current_user_id;
+    $current_user_id = $result[0]["user_id"];
+  }
+  else return false;
+}
+
+function getUserId() {
+  return $current_user_id;
+}
+
+function userNameCorrect($username) {
+  return true;
+}
+
+function getUserName() {
+  if(isset($_COOKIE["username"])) {
+    return $_COOKIE["username"];
+  }
+  else {
+    return false;
+  }
+}
+
+function setUsername($username) {
+  if(userNameCorrect($username)) {
+    setcookie("username", $username, time()+3600);
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 ?>
