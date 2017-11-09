40dfb4fbfa040b1a750fe53951ce68e1f22ef4d6
leevironty <leevi.ronty@gmail.com>
2017-11-09 18:42:48 +0200

    Login page

---
+++ siteLogin.php
@@ -0,0 +1,8 @@
+<div class="login">
+  <form>
+    <p> Kirjaudu ensin sisään</p>
+    <input id="user" type="text" placeholder="Käyttjätunnus">
+    <input id="password" type="password" placeholder="Salasana">
+    <input type="submit">
+  </form>
+</div>
