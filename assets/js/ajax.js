7ba42e6c7143a9084bb1ab6a97b44f88253c7a06
leevironty <leevi.ronty@gmail.com>
2018-01-20 16:13:21 +0200

    Load items on pageready

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -11,21 +11,30 @@ var varastoData = {
   },
   updateLists: function(){
     if (this.items.length>0 && this.classes.length>0){
+      console.log('Alkutesti läpi, jatketaan...')
       c = {};
       for(i=0;i<this.classes.length;i++){
         c[this.classes[i].id]={className:this.classes[i].name,classItems:Array()};
       }
-      for((i=0;i<this.items.length;i++){
-        c[this.items[i].class_id].classItems.append(this.items[i]);
+      for(i=0;i<this.items.length;i++){
+        c[this.items[i].class_id].classItems.push(this.items[i]);
       }
-
+      console.log(c)
       for(i=0;i<this.classes.length;i++){
-        current = c[this.classes[i].id]
-        template = $('#item-group-sample').html()
-        $(template).
+        current = c[this.classes[i].id];
+        console.log('index: '+i+', current: ')
+        console.log(current)
+        template = $('#item-group-sample').children('.item-group').clone();
+        template.find('.counter').text(current.classItems.length);
+        lista = template.find('.item-list>ul');
+        listaHTML = ""
+        for(j=0;j<current.classItems.length;j++){
+          listaHTML += '<li className="unique-item">'+current.name+' #'+current.classItems[j].id+'</li>';
+        }
+        lista.html(listaHTML);
       }
     }
-  };
+  }
 }
 
 
